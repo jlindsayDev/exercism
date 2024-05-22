92270171e7834cf8a1b593716b080bdfbd2103cb
Josh Lindsay <652996-jlindsayDev@users.noreply.gitlab.com>
2024-05-22 16:39:50 -0700

    [python] prime-factors solution

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,24 @@
+from math import sqrt
+
 def factors(value):
-    pass
+    primes = primes_generator()
+    factor_list = []
+
+    num = next(primes)
+    while value >= num:
+        if value % num == 0:
+            factor_list.append(num)
+            value //= num
+        else:
+            num = next(primes)
+
+    return factor_list
+
+def primes_generator():
+    yield 2
+
+    num = 1
+    while True:
+        num += 2
+        if not any(num % n == 0 for n in range(3, int(sqrt(num)), 2)):
+            yield num
