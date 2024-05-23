f5ce6045c8a567f660d9223e22f1d5491d1e81e1
Josh Lindsay <652996-jlindsayDev@users.noreply.gitlab.com>
2024-05-23 13:15:33 -0700

    [python] prime-factors remove dead code

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,43 +1,16 @@
-from math import sqrt, log
-
 def factors(value):
     factor_list = []
 
-    # reduce even numbers to odd primes
     while value % 2 == 0:
         factor_list.append(2)
         value //= 2
 
-    # no need to compute primes since we know we'll never encounter lower values
-    num = 3
-    while value >= num:
-        if value % num == 0:
-            factor_list.append(num)
-            value //= num
+    factor = 3
+    while value >= factor:
+        if value % factor == 0:
+            factor_list.append(factor)
+            value //= factor
         else:
-            num += 2
+            factor += 2
 
     return factor_list
-
-def old_factors(value):
-    primes = primes_generator()
-    factor_list = []
-
-    num = next(primes)
-    while value >= num:
-        if value % num == 0:
-            factor_list.append(num)
-            value //= num
-        else:
-            num = next(primes)
-
-    return factor_list
-
-def primes_generator():
-    yield 2
-
-    num = 1
-    while True:
-        num += 2
-        if not any(num % n == 0 for n in range(3, int(sqrt(num)), 2)):
-            yield num
