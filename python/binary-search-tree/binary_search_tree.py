9dd4e15441391d8f2b418c60d18beb401e9252c3
Josh Lindsay <652996-jlindsayDev@users.noreply.gitlab.com>
2024-05-23 10:55:02 -0700

    [python] binary-search-tree solution

--- python/binary-search-tree/binary_search_tree.py
+++ python/binary-search-tree/binary_search_tree.py
@@ -1,8 +1,8 @@
 class TreeNode:
     def __init__(self, data, left=None, right=None):
-        self.data = None
-        self.left = None
-        self.right = None
+        self.data = data
+        self.left = left
+        self.right = right
 
     def __str__(self):
         return f'TreeNode(data={self.data}, left={self.left}, right={self.right})'
@@ -10,10 +10,32 @@ class TreeNode:
 
 class BinarySearchTree:
     def __init__(self, tree_data):
-        pass
+        def insert(node, value):
+            if node == None:
+                node = TreeNode(value)
+            elif value <= node.data:
+                node.left = insert(node.left, value)
+            else:
+                node.right = insert(node.right, value)
+            return node
+
+        self.head = None
+
+        for value in tree_data:
+            self.head = insert(self.head, value)
 
     def data(self):
-        pass
+        return self.head
 
     def sorted_data(self):
-        pass
+        tree_data = []
+
+        def traverse(node):
+            if node == None:
+                return
+            traverse(node.left)
+            tree_data.append(node.data)
+            traverse(node.right)
+
+        traverse(self.head)
+        return tree_data
