2c6888b255516b5c415c346107ebbc082b465068
Josh Lindsay <652996-jlindsayDev@users.noreply.gitlab.com>
2024-05-23 22:49:57 -0700

    [typescript] resistor-color improvement

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,3 @@
-export const colorCode = (color : string) => COLORS.indexOf(color);
-
 export const COLORS = [
   'black',
   'brown',
@@ -12,3 +10,5 @@ export const COLORS = [
   'grey',
   'white',
 ]
+
+export const colorCode = COLORS.indexOf
