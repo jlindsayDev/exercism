546f1023aede767e7c0f7b46581dfc45d6c5dfc8
Josh Lindsay <652996-jlindsayDev@users.noreply.gitlab.com>
2024-05-23 23:15:57 -0700

    [typescript] resistor-color-trio implementation

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,23 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export const COLORS: string[] = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+]
+
+export const decodedResistorValue = (colors: string[]) : string => {
+  let reduceFn = (value: number, color: string) => value * 10 + COLORS.indexOf(color)
+  let sum: number = colors.slice(0, 2).reduce(reduceFn, 0)
+  sum *= 10 ** COLORS.indexOf(colors[2])
+
+  if (sum > 10**9) return `${sum / 10**9} gigaohms`
+  if (sum > 10**6) return `${sum / 10**6} megaohms`
+  if (sum > 10**3) return `${sum / 10**3} kiloohms`
+  return `${sum} ohms`
 }
