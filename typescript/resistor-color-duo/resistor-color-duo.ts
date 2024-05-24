e0d5ac272ad308f8979909b0084ea7d738152fdc
Josh Lindsay <652996-jlindsayDev@users.noreply.gitlab.com>
2024-05-23 22:49:40 -0700

    [typescript] resistor-color-duo solution

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,16 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
-}
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+]
+
+export const decodedValue = (colors: string[]) : number => (
+  colors.slice(0, 2).reduce((value, color) => value * 10 + COLORS.indexOf(color), 0)
+)
