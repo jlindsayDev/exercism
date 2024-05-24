03ba8744c967b7c8a3db6b79eaa8a9ef83bdb7f8
Josh Lindsay <652996-jlindsayDev@users.noreply.gitlab.com>
2024-05-23 23:39:37 -0700

    [typescript] linked-list solution

--- typescript/linked-list/linked-list.ts
+++ typescript/linked-list/linked-list.ts
@@ -1,25 +1,29 @@
 export class LinkedList<TElement> {
-  public push(element: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  private elements: TElement[] = []
+
+  public push(element: TElement): void {
+    this.elements.push(element)
   }
 
-  public pop(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public pop(): TElement | undefined {
+    return this.elements.pop()
   }
 
-  public shift(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public shift(): TElement | undefined {
+    return this.elements.shift()
   }
 
-  public unshift(element: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  public unshift(element: TElement): number {
+    return this.elements.unshift(element)
   }
 
-  public delete(element: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  public delete(element: TElement): void {
+    const elementIndex: number = this.elements.indexOf(element)
+    if (elementIndex >= 0)
+      this.elements.splice(elementIndex, 1)
   }
 
-  public count(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public count(): number {
+    return this.elements.length
   }
 }
